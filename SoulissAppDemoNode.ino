e1a256922d1cd00cb94392b2df251de2b0d24a09
shineangelic <shineangelic@gmail.com>
2019-11-06 01:12:12 +0100

    First commit

---
+++ SoulissAppDemoNode.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SoulissAppDemoNode.ino
+ Created:	06/11/2019 01:08:21
+ Author:	Ale
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
